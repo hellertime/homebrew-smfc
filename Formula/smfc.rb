54507bbb4e67a367fcffa81971b7a47b7c75a974
Chris Heller <chris.heller@datadoghq.com>
2020-10-12 23:55:31 -0400

    [smfc] brew formula

---
+++ Formula/smfc.rb
@@ -0,0 +1,23 @@
+class Smfc < Formula
+  desc "SMF (the fastest RPC in the West) IDL Compiler"
+  homepage "https://github.com/hellertime/smfc"
+  url "https://github.com/hellertime/smfc/archive/2010.10.03.zip"
+  sha256 "b779170d765eaaf2ca5f84a301e8590586ce1ee4c1a7d8d3b7fdb4cbedb84bce"
+  license "Apache-2.0"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "flatbuffers" 
+  depends_on "gflags" 
+  depends_on "glog" 
+  depends_on "boost"
+
+  def install
+    system "cmake","."
+    system "make"
+    system "make", "install"
+  end
+
+  def test
+    system "false"
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Smfrpc Smfc
+
+## How do I install these formulae?
+`brew install smfrpc/smfc/<formula>`
+
+Or `brew tap smfrpc/smfc` and then `brew install <formula>`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
